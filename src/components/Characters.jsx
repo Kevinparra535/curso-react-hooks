79f4c506a62b18135971405427b8e88c3a2e2bf3
Kevin <kevinparra535@gmail.com>
2021-06-29 08:13:37 -0500

    [ADD] useReducer

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,7 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
+
+// Creamos un estado inicial
+const initialState = {
+  favorites: [],
+};
+
+// creamos el reducer que es el que se encargar de agregarlo
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_FAVORITE":
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
   // Se le pasan dos parametros, el primero será una funcion anonima y el segundo una variable que estará escuchando en base a los cambios
   useEffect(() => {
@@ -10,10 +31,23 @@ const Characters = () => {
       .then((data) => setCharacters(data.results));
   }, []);
 
+  const handleClick = (favorite) => {
+    dispatch({ type: "ADD_TO_FAVORITE", payload: favorite });
+  };
+  console.log(favorites);
   return (
     <div className="Characters">
+      {favorites.favorites.map((favorite) => (
+        <li key={favorite.id}>{favorite.name}</li>
+      ))}
+
       {characters.map((character) => (
-        <h2>{character.name}</h2>
+        <div className="item" key={character.id}>
+          <h2>{character.name}</h2>
+          <button type="button" onClick={() => handleClick(character)}>
+            Agregar a favoritos
+          </button>
+        </div>
       ))}
     </div>
   );
