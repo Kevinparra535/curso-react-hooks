403b6f3b3d717f5112effe248dbdebdd084baa3f
Kevin <kevinparra535@gmail.com>
2021-07-02 08:18:34 -0500

    [ADD] useMemo

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useMemo } from "react";
 
 // Creamos un estado inicial
 const initialState = {
@@ -21,10 +21,12 @@ const favoriteReducer = (state, action) => {
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
-
   const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
-  // Se le pasan dos parametros, el primero será una funcion anonima y el segundo una variable que estará escuchando en base a los cambios
+  // Este Hook nos va a almacenar la búsqueda
+  const [search, setSearch] = useState("");
+
+  // Se le pasan dos parametros, el primero será una función anónima y el segundo una variable que estará escuchando en base a los cambios
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character/")
       .then((response) => response.json())
@@ -34,14 +36,46 @@ const Characters = () => {
   const handleClick = (favorite) => {
     dispatch({ type: "ADD_TO_FAVORITE", payload: favorite });
   };
-  console.log(favorites);
+
+  // Nos detecta los valores en el input
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // Filtro
+  // const filteredUsers = characters.filter((user) => {
+  //   return user.name.toLowerCase().includes(search.toLowerCase());
+  // });
+
+  // Filtro con Use Memo
+
+  const filteredUsers = useMemo(
+    () =>
+      characters.filter((user) => {
+        return user.name.toLowerCase().includes(search.toLowerCase());
+      }),
+    [characters, search]
+  );
+
   return (
     <div className="Characters">
       {favorites.favorites.map((favorite) => (
         <li key={favorite.id}>{favorite.name}</li>
       ))}
 
-      {characters.map((character) => (
+      <div>
+        <label>
+          Búsqueda:
+          <input
+            name="search"
+            type="text"
+            value={search}
+            onChange={handleSearch}
+          ></input>
+        </label>
+      </div>
+
+      {filteredUsers.map((character) => (
         <div className="item" key={character.id}>
           <h2>{character.name}</h2>
           <button type="button" onClick={() => handleClick(character)}>
