ff1c91006ea8bc8aad734c9e99e90e12c1eadf71
Kevin Parra <kevinparra535@gmail.com>
2021-07-13 08:41:18 -0500

    [ADD] Conectando el flujo de la app y agregando iconos

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../../styles/components/Layout/Header.css';
 
 const Header = () => (
   <div className="Header">
-    <h1 className="Header-title">PlatziConf Merch</h1>
-    <div className="Header-checkout">Checkout</div>
+    <Link to="/">
+      <h1 className="Header-title">PlatziConf Merch</h1>
+    </Link>
+    <div className="Header-checkout">
+      <Link to="/checkout">
+        <i className="fas fa-shopping-basket" title="Checkout" />
+      </Link>
+    </div>
   </div>
 );
 
