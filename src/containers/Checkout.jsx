f2b9928adfd90901a933d1815a2cbb2f664d84a3
Kevin Parra <kevinparra535@gmail.com>
2021-09-13 18:34:16 -0500

    [ADD] useRef para el formulairo de pago

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,37 +1,27 @@
-/* eslint-disable no-unused-vars */
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
 import AppContext from '../hooks/Context/AppContext';
-
 import '../styles/components/Checkout.css';
 
 const Checkout = () => {
-  const {
-    state: { cart },
-    removeFromCart,
-  } = useContext(AppContext);
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
 
-  // Remuve from cart
   const handleRemove = (product) => () => {
     removeFromCart(product);
   };
 
-  // Sumatoria total
   const handleSumTotal = () => {
     const reducer = (accumulator, currentValue) =>
       accumulator + currentValue.price;
-
     const sum = cart.reduce(reducer, 0);
-
     return sum;
   };
 
   return (
     <div className="Checkout">
       <div className="Checkout-content">
-        <h3>{cart.length > 0 ? 'Lista de Pedidos:' : 'Sin pedidos'}</h3>
-
+        {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3>}
         {cart.map((item) => (
           <div className="Checkout-item">
             <div className="Checkout-element">
@@ -39,12 +29,11 @@ const Checkout = () => {
               <span>${item.price}</span>
             </div>
             <button type="button" onClick={handleRemove(item)}>
-              <i className="fas fa-trash-alt" title="Eliminar" />
+              <i className="fas fa-trash-alt" />
             </button>
           </div>
         ))}
       </div>
-
       {cart.length > 0 && (
         <div className="Checkout-sidebar">
           <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,81 +1,134 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable react/destructuring-assignment */
+import React, { useRef, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import AppContext from '../hooks/Context/AppContext';
 
 import '../styles/components/Information.css';
 
-const Information = () => (
-  <div className="Information">
-    <div className="Information-content">
-      <div className="Information-head">
-        <h2>Información de contacto:</h2>
-      </div>
-      <div className="Information-form">
-        <form action="">
-          <label htmlFor="name">
-            Full Name
-            <input type="text" placeholder="Full Name" name="name" id="name" />
-          </label>
-          <label htmlFor="email">
-            Email
-            <input type="text" placeholder="Email" name="email" id="email" />
-          </label>
-          <label htmlFor="address">
-            Address
-            <input
-              type="text"
-              placeholder="Address"
-              name="address"
-              id="address"
-            />
-          </label>
-          <label htmlFor="apto">
-            APT
-            <input type="text" placeholder="APT" name="apto" id="apto" />
-          </label>
-          <label htmlFor="country">
-            Country
-            <input
-              type="text"
-              placeholder="Country"
-              name="country"
-              id="country"
-            />
-          </label>
-          <label htmlFor="state">
-            State
-            <input type="text" placeholder="State" name="state" id="state" />
-          </label>
-          <label htmlFor="city">
-            City
-            <input type="text" placeholder="City" name="city" id="city" />
-          </label>
-          <label htmlFor="cp">
-            Postal Code
-            <input type="text" placeholder="Postal Code" name="cp" id="cp" />
-          </label>
-          <label htmlFor="phone">
-            Phone
-            <input type="text" placeholder="Prone" name="phone" id="phone" />
-          </label>
-        </form>
-      </div>
-      <div className="Information-buttons">
-        <div className="Information-back">Regresar</div>
-        <div className="Information-next">
-          <Link to="/checkout/payment">pagar</Link>
+const Information = (props) => {
+  console.log('Information');
+
+  const { state, addToBuyer } = useContext(AppContext);
+
+  const form = useRef(null);
+
+  const { cart } = state;
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    const buyer = {
+      name: formData.get('name'),
+      email: formData.get('email'),
+      phone: formData.get('phone'),
+      address: formData.get('address'),
+      apto: formData.get('apto'),
+      state: formData.get('state'),
+      city: formData.get('city'),
+      country: formData.get('country'),
+      cp: formData.get('cp'),
+    };
+
+    // formData.forEach((value, key) => {
+    //   buyer[key] = value;
+    // });
+
+    console.log(buyer);
+    history.push('/checkout/payment');
+
+    addToBuyer(buyer);
+  };
+
+  return (
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Información de contacto:</h2>
+        </div>
+        <div className="Information-form">
+          <form ref={form}>
+            <label htmlFor="name">
+              Full Name
+              <input
+                type="text"
+                placeholder="Full Name"
+                name="name"
+                id="name"
+              />
+            </label>
+            <label htmlFor="email">
+              Email
+              <input type="text" placeholder="Email" name="email" id="email" />
+            </label>
+            <label htmlFor="address">
+              Address
+              <input
+                type="text"
+                placeholder="Address"
+                name="address"
+                id="address"
+              />
+            </label>
+            <label htmlFor="apto">
+              APT
+              <input type="text" placeholder="APT" name="apto" id="apto" />
+            </label>
+            <label htmlFor="country">
+              Country
+              <input
+                type="text"
+                placeholder="Country"
+                name="country"
+                id="country"
+              />
+            </label>
+            <label htmlFor="state">
+              State
+              <input type="text" placeholder="State" name="state" id="state" />
+            </label>
+            <label htmlFor="city">
+              City
+              <input type="text" placeholder="City" name="city" id="city" />
+            </label>
+            <label htmlFor="cp">
+              Postal Code
+              <input type="text" placeholder="Postal Code" name="cp" id="cp" />
+            </label>
+            <label htmlFor="phone">
+              Phone
+              <input type="text" placeholder="Prone" name="phone" id="phone" />
+            </label>
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">
+            <button type="button">Regresar</button>
+          </div>
+          <div className="Information-next">
+            <button onClick={handleSubmit} type="button">
+              Pagar
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="Information-sidebar">
-      <h3>Pedido:</h3>
-      <div className="Information-item">
-        <div className="Information-element">
-          <h4>ITEM Name</h4>
-          <span>$10</span>
-        </div>
+      <div className="Information-sidebar">
+        <h3>Pedido:</h3>
+        {cart.map((item) => (
+          <div className="Information-item" key={item.title}>
+            <div className="Information-element">
+              <h4>{item.title}</h4>
+              <span>${item.price}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Information;
